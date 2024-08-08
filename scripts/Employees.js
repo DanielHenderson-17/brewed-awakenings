d8276bb9562a7865262e89ab1a93d60ab1eb09bf
Daniel Henderson <djhindu17@gmail.com>
2024-08-08 13:44:57 -0500

    Show how much employees sold

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,16 +1,26 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
+
+document.addEventListener("click", (clickEvent) => {
+    const employeeTarget = clickEvent.target
+    if (employeeTarget.dataset.type === "employee") {
+        const employeeId = parseInt(employeeTarget.dataset.employeeid)
+        const employeeOrders = orders.filter(order => order.employeeId === employeeId)
+        const productsSold = employeeOrders.length
+        window.alert(`${employeeTarget.textContent} has sold ${productsSold} products`)
+    }
+})
 
 export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li>${employee.name}</li>`
+        html += `<li data-type="employee" data-employeeid="${employee.id}">${employee.name}</li>`
     }
 
     html += "</ul>"
 
     return html
 }
-
